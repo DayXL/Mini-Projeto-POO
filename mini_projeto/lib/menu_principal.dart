f0695e690cb6e75ad234ea23ffdc888bef559204
DayXL <day.luc.xav0606@gmail.com>
2023-05-24 21:28:15 -0300

    começado estilo de tela principal

--- mini_projeto/lib/menu_principal.dart
+++ mini_projeto/lib/menu_principal.dart
@@ -1,33 +1,61 @@
 import 'package:flutter/material.dart';
 
+late final GlobalKey<ScaffoldMessengerState> _scaffoldMessengerKey;
+
 void main() {
-  runApp(const MyApp());
+
+  _scaffoldMessengerKey = GlobalKey<ScaffoldMessengerState>();
+
+  MyApp app = const MyApp();
+
+  runApp(app);
+
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+
   @override
+
   Widget build(BuildContext context) {
+
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+
+      debugShowCheckedModeBanner:false,
+
+      home: ScaffoldMessenger(
+
+        key: _scaffoldMessengerKey,
+
+        child: Scaffold(
+          backgroundColor: Colors.black,
+
+          body: Center(
+            child: Container(
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [Colors.greenAccent, Colors.black],
+                  stops: [0.2, 0.3], 
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                ),
+                
+              ),
+
+              
+            ),
+
+          ),
+
+        )
+      )
     );
+
   }
+
 }
+
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
