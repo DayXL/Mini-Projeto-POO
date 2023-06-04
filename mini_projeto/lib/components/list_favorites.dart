ca9d07b2cbc11cb523d0eeaf2e02c0cb5b2a169d
unknown <charleseduardofaria@gmail.com>
2023-06-03 21:49:47 -0300

    Tratando JSON vazio na tela de favoritos

--- mini_projeto/lib/components/list_favorites.dart
+++ mini_projeto/lib/components/list_favorites.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import '../services/game_services.dart';
@@ -15,51 +17,57 @@ class ListFavorites extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView(children: <Widget>[
-      const SizedBox(height: 20),
-      const Center(
-        child: Text("Lista de Favoritos",
-            style: TextStyle(fontSize: 20, color: Colors.white)),
-      ),
-      const SizedBox(height: 20),
-      jsonObjects.isEmpty
-          ? const Center(
-              child: Text("Você ainda não adicionou nenhum favorito",
+    return jsonObjects.isEmpty
+        ? const Center(
+            child: Text("Você ainda não adicionou nenhum favorito",
+                style: TextStyle(fontSize: 20, color: Colors.white)),
+          )
+        : ListView(children: <Widget>[
+            const SizedBox(height: 20),
+            const Center(
+              child: Text("Lista de Favoritos",
                   style: TextStyle(fontSize: 20, color: Colors.white)),
-            )
-          : Column(
-              children: jsonObjects.map(
-              (jsonObject) {
-                return Card(
-                  child: ListTile(
-                    leading: Image.network(
-                      jsonObject[propertyNames[2]],
-                      height: 150,
-                      scale: 0.4,
-                    ),
-                    title: Text(jsonObject[propertyNames[0]]),
-                    subtitle: Text(jsonObject[propertyNames[1]]),
-                    trailing: IconButton(
-                      icon: const Icon(Icons.favorite),
-                      color: Colors.red,
-                      onPressed: () {
-                        gameService.favoriteStateNotifier.value = {
-                          'status': ConnectionStatus.ready,
-                          'dataObjects': gameService
-                              .favoriteStateNotifier.value['dataObjects']
-                              .where((element) =>
-                                  element[propertyNames[0]] !=
-                                  jsonObject[propertyNames[0]])
-                              .toList(),
-                          'propertyNames': gameService
-                              .favoriteStateNotifier.value['propertyNames'],
-                        };
-                      },
-                    ),
-                  ),
-                );
-              },
-            ).toList()),
-    ]);
+            ),
+            const SizedBox(height: 20),
+            jsonObjects.isEmpty
+                ? const Center(
+                    child: Text("Você ainda não adicionou nenhum favorito",
+                        style: TextStyle(fontSize: 20, color: Colors.white)),
+                  )
+                : Column(
+                    children: jsonObjects.map(
+                    (jsonObject) {
+                      return Card(
+                        child: ListTile(
+                          leading: Image.network(
+                            jsonObject[propertyNames[2]],
+                            height: 150,
+                            scale: 0.4,
+                          ),
+                          title: Text(jsonObject[propertyNames[0]]),
+                          subtitle: Text(jsonObject[propertyNames[1]]),
+                          trailing: IconButton(
+                            icon: const Icon(Icons.favorite),
+                            color: Colors.red,
+                            onPressed: () {
+                              gameService.favoriteStateNotifier.value = {
+                                'status': ConnectionStatus.ready,
+                                'dataObjects': gameService
+                                    .favoriteStateNotifier.value['dataObjects']
+                                    .where((element) =>
+                                        element[propertyNames[0]] !=
+                                        jsonObject[propertyNames[0]])
+                                    .toList(),
+                                'propertyNames': gameService
+                                    .favoriteStateNotifier
+                                    .value['propertyNames'],
+                              };
+                            },
+                          ),
+                        ),
+                      );
+                    },
+                  ).toList()),
+          ]);
   }
 }
